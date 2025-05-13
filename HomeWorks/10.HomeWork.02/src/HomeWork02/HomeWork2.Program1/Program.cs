c24dccaa91d33661758450be8840ee741a4fd70a
proninp <proninpaul@yandex.ru>
2025-05-13 22:41:10 +0300

    Lesson 10 - HomeWork 02 - Program 1

---
+++ HomeWorks/10.HomeWork.02/src/HomeWork02/HomeWork2.Program1/Program.cs
@@ -0,0 +1,25 @@
+﻿var files = new[] { "NewFile1.txt", "NewFile2.txt", "NewFile3.txt" };
+
+var tasks = new List<Task>(files.Length);
+
+foreach (var file in files)
+{
+    var path = Path.Combine(AppContext.BaseDirectory, "Resources", file);
+    
+    tasks.Add(Task.Run(() =>
+    {
+        var spaces = CountSpaces(path);
+        Console.WriteLine($"Пробелов в файле {file}: {spaces}");
+    }));
+}
+
+await Task.WhenAll(tasks.ToArray());
+return;
+
+int CountSpaces(string path)
+{
+    return File 
+        .ReadLines(path)
+        .SelectMany(s => s.ToCharArray())
+        .Count(c => c == ' ');
+}
